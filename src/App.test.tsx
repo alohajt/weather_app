61ba71ae13e3eeaff7a83b50a90bafe01470e6db
alohajt <alohajiatao@gmail.com>
2022-03-25 14:44:02 -0400

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { useEffect, useState } from "react";
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=313df0952d0a44e38ca0118cca3f0863";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+        <p>{advice}</p>
+);
+
+};
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { EventHandler, useEffect, useState } from "react";
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+import { type WeatherData } from "./service/weather-data";
+// type WeatherData = { weather: Array<{ description: String }> }
 
-function App() {
+const App = () => {
+
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [cityName, setCityName] = useState<string>("");
+
+
+  const fetchData = async (city: string) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=313df0952d0a44e38ca0118cca3f0863`;
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      setWeatherData(json);
+
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData("boston");
+  }, []);
+
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    fetchData(cityName);
+    event.preventDefault();
+
+  }
+
+  const handleTextChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setCityName(event.target.value)
+    event.preventDefault();
+
+  }
+  if (!weatherData) {
+    return <div>
+      loading
+    </div>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{weatherData.name} </h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={cityName} onChange={handleTextChange} />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h2>Temp: </h2>
+      <h2>{weatherData.weather && weatherData.weather[0].description}</h2>
     </div>
   );
-}
+
+};
 
 export default App;
+
