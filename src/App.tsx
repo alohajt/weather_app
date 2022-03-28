59c785ee2e56cb650e8cd970342bec9bfa5840a7
alohajt <alohajiatao@gmail.com>
2022-03-28 14:13:18 -0400

    added react mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { EventHandler, useEffect, useState } from "react";
 import { type WeatherData } from "./service/weather-data";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+import Typography from '@mui/material/Typography';
+
 
 const App = () => {
 
@@ -39,21 +44,42 @@ const App = () => {
     </div>
   }
 
-  console.log("weatherData.weather[0].main",weatherData);
-  
+  console.log("weatherData.weather[0].main", weatherData);
+  // const temp = (weatherData.main && weatherData.main.temp) − 273.15
+  if (weatherData.main && weatherData.main.temp) {
+    const temp = Math.ceil(weatherData.main.temp - 273.15)
+    console.log("temp", temp);
+  }
+
+  const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQst8vcOxn0BCuQLyVoSdCrNlEuTwJskeJzbQ&usqp=CAU"
+  const styles = {
+    paperContainer: {
+        backgroundImage: `url(${image})`
+    }
+};
+
   return (
     <div>
-      <h1>{weatherData.name} </h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={cityName} onChange={handleTextChange} />
-        <input type="submit" value="Submit" />
-      </form>
+      <Paper 
+      style={styles.paperContainer}>
+        <Typography variant="h2" component="h2" align="center">
+          {weatherData.name}
+        </Typography>
 
-      <h2>Temp: {weatherData.main && weatherData.main.temp} K</h2>      
-      <h2>{weatherData.weather && weatherData.weather[0].description}</h2>
-      <h2>Max Temp: {weatherData.main && weatherData.main.temp_max} K</h2>
-      <h2>Min Temp: {weatherData.main && weatherData.main.temp_min} K</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={cityName} onChange={handleTextChange} />
+          <input type="submit" value="Submit" />
+        </form>
 
+        {/* <h2>Temp: {temp} C</h2>       */}
+        <Typography variant="body1" component="h3" align="center">
+          {weatherData.weather && weatherData.weather[0].description}
+        </Typography>
+        <Typography variant="body1" component="h3" align="center">
+          Max Temp:{weatherData.main && weatherData.main.temp_max} K <br />
+          Min Temp: {weatherData.main && weatherData.main.temp_min} K
+        </Typography>
+      </Paper>
     </div>
   );
 
